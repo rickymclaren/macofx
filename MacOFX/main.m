91d5312f76d194df271b75f90b52c395a91bfe6e
Ricky McLaren <rickymclaren@gmail.com>
2012-09-11 23:19:49 +0100

    Initial Commit

---
+++ MacOFX/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MacOFX
+//
+//  Created by Richard McLaren on 11/09/2012.
+//  Copyright (c) 2012 Richard McLaren. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
