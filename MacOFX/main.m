e9a018645c311f306b52bffe96a079a9f2e0a7b3
Ricky McLaren <rickymclaren@gmail.com>
2012-09-13 18:17:12 +0100

    Open fox and parse

--- MacOFX/main.m
+++ MacOFX/main.m
@@ -13,8 +13,57 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString *home = NSHomeDirectory();
+        NSLog(@"%@", home);
+        
+        NSFileManager* sharedFM = [NSFileManager defaultManager];
+        NSArray* possibleURLs = [sharedFM URLsForDirectory:NSDownloadsDirectory
+                                                 inDomains:NSUserDomainMask];
+        NSURL* downloadDir = nil;
+        
+        if ([possibleURLs count] >= 1) {
+            // Use the first directory (if multiple are returned)
+            downloadDir = [possibleURLs objectAtIndex:0];
+        }
+        NSLog(@"%@", downloadDir);
+        
+        NSError *error = nil;
+        NSArray *properties = [NSArray arrayWithObjects:NSURLLocalizedNameKey, NSURLAttributeModificationDateKey, nil];
+        NSArray *files = [sharedFM contentsOfDirectoryAtURL:downloadDir
+                                includingPropertiesForKeys:properties
+                                options:NSDirectoryEnumerationSkipsHiddenFiles
+                                error:&error];
+        
+
+        NSString *ofxFile = nil;
+        for (NSURL *url in files) {
+            NSString *filePath = [url relativePath];
+            if ([filePath hasSuffix:@".ofx"]) {
+                NSLog(@"%@", filePath );
+                ofxFile = filePath;
+            }
+            
+        }
+        
+        error = nil;
+        if (ofxFile) {
+            NSString *contents = [NSString stringWithContentsOfFile:ofxFile encoding:NSUTF8StringEncoding error:&error];
+            if (error) {
+                NSLog(@"%@", error );
+                
+            } else {
+                NSRange range = [contents rangeOfString:@"<OFX>"];
+                contents = [contents substringFromIndex:range.location];
+
+                NSXMLDocument *doc = [[NSXMLDocument alloc] initWithXMLString:contents options:0 error:&error];
+                
+                if (error) {
+                    NSLog(@"%@", error );
+                } else {
+                    NSLog(@"%@", doc );
+                }
+            }
+        }
         
     }
     return 0;
