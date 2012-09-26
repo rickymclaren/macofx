a786f83e7aee1787026c2ff78ccf292e8fd0af34
Ricky McLaren <rickymclaren@gmail.com>
2012-09-26 23:30:06 +0100

    Add Filter

--- MacOFX/main.m
+++ MacOFX/main.m
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "OFXLoader.h"
 #import "OFXParserDOM.h"
+#import "Transaction.h"
 
 int main(int argc, const char * argv[])
 {
@@ -17,8 +18,25 @@ int main(int argc, const char * argv[])
         
         
         NSString *xml = [[[OFXLoader alloc] init] loadXML];
-        NSArray* transactions = [[[OFXParserDOM alloc] init] parsefromXMLString:xml];
-        NSLog(@"Transactions %@", transactions);
+        NSArray *transactions = [[[OFXParserDOM alloc] init] parsefromXMLString:xml];
+        BOOL all = true;
+        NSString *filter = @"";
+        if (argc == 2) {
+            all = false;
+            filter = [NSString stringWithCString:argv[1] encoding: NSASCIIStringEncoding];
+            
+        }
+        
+        double total = 0.0;
+        for (Transaction* t in transactions) {
+            if (all || [t.name hasPrefix:filter]) {
+                printf("%s\n", [t.description UTF8String]);
+                total += t.amount;
+                
+            }
+            
+        }
+        printf("Total %10.2f\n", total);
         
     }
     return 0;
